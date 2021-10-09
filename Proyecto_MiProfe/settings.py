739a9ca50839d216312d2659f5b7d8a6979561a1
Henry Mantilla <dario30agosto@gmail.com>
2021-10-09 17:09:10 -0500

    Entorno Django
    Protect Secret key

--- Proyecto_MiProfe/settings.py
+++ Proyecto_MiProfe/settings.py
@@ -19,8 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+#Proteger llave secreta 9/10/2021
+from dotenv import load_dotenv
+load_dotenv()
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tj8u!i!-4qg5-6neag21dgu^om$rcu@!k#-f&122h8l!-xf7(^'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
